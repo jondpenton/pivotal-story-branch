0d297c2bba712192d2a4da89338eb3bf9d62ba2e
Jon Penton <jon@penton.site>
2020-09-06 02:17:05 -0400

    test: :white_check_mark: Added tests

---
+++ src/lib/get-story-id.test.ts
@@ -0,0 +1,17 @@
+import { getStoryId } from "./get-story-id";
+
+it.each([
+  "https://www.pivotaltracker.com/story/show/12345678",
+  "http://www.pivotaltracker.com/story/show/12345678",
+])("%s", (link) => {
+  const expected = "12345678";
+  const actual = getStoryId(link);
+
+  expect(actual).toBe(expected);
+});
+
+it("throws an error if link is invalid", () => {
+  const link = "https://something.else";
+
+  expect(() => getStoryId(link)).toThrow();
+});
