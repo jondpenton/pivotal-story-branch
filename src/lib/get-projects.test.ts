97ffe65925d53ecbca1b622968388e7a54fac537
Jon Penton <jon@penton.site>
2020-09-07 13:02:28 -0400

    test: :white_check_mark: Added getProjects test

---
+++ src/lib/get-projects.test.ts
@@ -0,0 +1,25 @@
+import { server, rest } from "../mocks/server";
+import { IProject, getProjects } from "./get-projects";
+
+it("fetches projects from Pivotal Tracker API", async () => {
+  const projects: IProject[] = [
+    {
+      id: 123,
+      name: "Some project",
+    },
+    {
+      id: 124,
+      name: "Another one",
+    },
+  ];
+  server.use(
+    rest.get(
+      "https://www.pivotaltracker.com/services/v5/projects",
+      (_req, res, ctx) => res(ctx.json(projects))
+    )
+  );
+
+  const fetchedProjects = await getProjects();
+
+  expect(fetchedProjects).toMatchObject(projects);
+});
