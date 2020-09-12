17fec7f9faaff6624887eef375b9fd3cdbd04a1a
Jon Penton <jon@penton.site>
2020-09-12 12:40:24 -0400

    test: :white_check_mark: Moved env variable reset to setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,21 @@
-import { server } from "./src/mocks/server";
+import { server } from './src/mocks/server'
+
+let originalToken = process.env.PIVOTAL_TRACKER_TOKEN
+
 // Establish API mocking before all tests.
-beforeAll(() => server.listen());
+beforeAll(() => server.listen())
+
+beforeEach(() => {
+  process.env.PIVOTAL_TRACKER_TOKEN = undefined
+})
+
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
-afterEach(() => server.resetHandlers());
-// Clean up after the tests are finished.
-afterAll(() => server.close());
+afterEach(() => server.resetHandlers())
+
+afterAll(() => {
+  // Clean up after the tests are finished.
+  server.close()
+
+  process.env.PIVOTAL_TRACKER_TOKEN = originalToken
+})
